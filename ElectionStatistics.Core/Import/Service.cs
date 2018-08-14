7b2fd731a8ae8f3f82468c188aea759b582fae5a
Artem Baikuzin <ybinzu@gmail.com>
2018-08-14 12:57:25 +0300

    Started Core and Tests projects. Upgrade projects to netcoreapp2.1 #25
    Started ElectionStatistics.Core.Import #25

---
+++ ElectionStatistics.Core/Import/Service.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using ElectionStatistics.Model;
+
+namespace ElectionStatistics.Core.Import
+{
+    public class Service
+    {
+        public void Execute(string csvFile, IQueryable<Mapping> mapping,
+            IQueryable<MappingLine> mappingLines)
+        {
+            
+        }
+    }
+}
